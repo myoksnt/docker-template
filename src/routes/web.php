0e44c62019364be5bdde4e00403642b6269b3d65
myoksnt <santa.miyaoka@gmail.com>
2024-05-23 15:32:42 +0900

    change

--- src/routes/web.php
+++ src/routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TodoController;
+use App\Http\Controllers\ContactController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +18,4 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/', [TodoController::class, 'index']);
