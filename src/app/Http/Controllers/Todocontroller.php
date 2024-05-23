90c2daa9a81f7b03fc0d91fc640e01c7a55d0226
myoksnt <santa.miyaoka@gmail.com>
2024-05-23 16:41:00 +0900

    change

--- src/app/Http/Controllers/Todocontroller.php
+++ src/app/Http/Controllers/Todocontroller.php
@@ -2,15 +2,14 @@
 
 namespace App\Http\Controllers;
 
-use App\Http\Controllers\TodoController;
 use App\Http\Requests\TodoRequest;
 use App\Models\Todo;
 use Illuminate\Http\Request;
 
-class Todocontroller extends Controller
+class TodoController extends Controller
 {
-    public function index()
-    {
-        return view('index');
-    }
+  public function index()
+  {
+    return view('index');
+  }
 }
